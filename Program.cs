049ff87a6c8bbebd94100881f15a176a75c2e917
Jeruel John <jeruelj@gmail.com>
2020-02-06 21:36:20 -0500

    Updated

--- Program.cs
+++ Program.cs
@@ -7,23 +7,28 @@ namespace Deliverable_4___Methods
         static void Main(string[] args)
         {
             {//Prompt user to enter valid name for entry and see it printed
-                Console.WriteLine("Please enter your name without numbers");
+                Console.WriteLine("Please enter your first and last name without numbers");
                 try
                 {
-                    //creates variable for user name entry
-                    string input = Console.ReadLine();
-                    decimal numbername = decimal.Parse(input);
-                    NameEntry(ref input);
+                    //enter the user input into the field
+                    string Name = Console.ReadLine();
 
-                    //Not allowing entry of a number in the user's name.
-                    if ((numbername >= 0) && (numbername <= 0))
-                        Console.WriteLine("Please enter a valid entry");
+                    if (System.Text.RegularExpressions.Regex.IsMatch(Name, "[a-zA-Z]" + " " + "[a-zA-Z]"))
+                    {
+                        NameEntry(ref Name);
+                    }
+
+                    else
+                    {
+                        // ask the user to enter a name without numbers
+                        Console.WriteLine("Please enter a valid entry that has your first and last name seperated by a space.");
+                    }
                 }
                 //End of try section
                 catch
                 {
                     //If an error occurs. Inform user to enter a different value. 
-                    Console.WriteLine("Please enter a valid entry.");
+                    Console.WriteLine("Please enter a valid entry that has your first and last name seperated by a space.");
                 }
                 //end of catch section
             }
